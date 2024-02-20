09c1dbc84e3cfb66c2eae674f84e6d7c73f55596
Rodrigo Marques Barbosa <minetalebr@gmail.com>
2024-02-19 22:01:23 -0300

    criei evento erro ou sucesso no cadastro

--- routes/admin.js
+++ routes/admin.js
@@ -20,30 +20,43 @@ router.get("/categorias/add", (req, res) => {
 router.post("/categorias/nova", (req, res) => {
   // **logica para verificar e criar avisos de erro de cadastro de categoria
   var erros = [];
-  if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
-    erros.push({texto: "Nome da categoria é invalido!"})
-  };
-  if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
-    erros.push({texto: "Slug da categoria é invalido!"})
-  };
-  if(req.body.nome.length < 2){
-    erros.push({texto: "Nome da categoria é muito pequeno!"})
-  };
-  if(erros.length > 0){
-    res.render("admin/addcategorias",{erros: erros})
-  };
-  // criando uma nova categoria no banco
-  const novaCategoria = {
-    nome: req.body.nome,
-    slug: req.body.slug,
-  };
-console.log(novaCategoria)
-  new Categoria(novaCategoria).save().then(() => {
-    console.log("Categoria salva com sucesso!")
-  }).catch((err) => {
-    console.log(`Erro ao criar categoria: ${err}`)
-  });
-
+  if (
+    !req.body.nome ||
+    typeof req.body.nome == undefined ||
+    req.body.nome == null
+  ) {
+    erros.push({ texto: "Nome da categoria é invalido!" });
+  }
+  if (
+    !req.body.slug ||
+    typeof req.body.slug == undefined ||
+    req.body.slug == null
+  ) {
+    erros.push({ texto: "Slug da categoria é invalido!" });
+  }
+  if (req.body.nome.length < 2) {
+    erros.push({ texto: "Nome da categoria é muito pequeno!" });
+  }
+  if (erros.length > 0) {
+    res.render("admin/addcategorias", { erros: erros });
+  } else {
+    // criando uma nova categoria no banco
+    const novaCategoria = {
+      nome: req.body.nome,
+      slug: req.body.slug,
+    };
+    console.log(novaCategoria);
+    new Categoria(novaCategoria)
+      .save()
+      .then(() => {
+        req.flash("success_msg", "Sucesso ao salvar a nova categoria!");
+        res.redirect("admin/categorias");
+      })
+      .catch((err) => {
+        req.flash("error_msg","Erro ao salvar a nova categoria!")
+        console.log(`Erro ao criar categoria: ${err}`);
+      });
+  }
 });
 // exportando as rotas para o projeto
 module.exports = router;
