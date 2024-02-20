03f6c112ad0f7c2abc0e36048b0b66939f59215e
Rodrigo Marques Barbosa <minetalebr@gmail.com>
2024-02-19 21:08:11 -0300

    comecei a criar a session

--- app.js
+++ app.js
@@ -6,8 +6,28 @@ const app = express();
 const admin = require("./routes/admin");
 const path = require("path");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // CONFIGURAÇÕES
+
+// Criando Sessão
+app.use(
+  session({
+    secret: "blogApp",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+// criando MiddleWare
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
+
 //body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
