ec92b09969953a17b0bb83de3346cf537fdc1fb0
Rodericuss <githubportfolio@outlook.com>
2024-04-09 08:29:00 -0300

    authentication and other things

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
-const { Console } = require("console");
 require("./models/Postagem")
 const Postagem = mongoose.model("postagens")
-
+require("./models/Categoria");
+const Categoria = mongoose.model("categorias");
+const usuarios = require('./routes/usuario')
+const passport = require('passport')
+require('./config/auth')(passport)
 // CONFIGURAÇÕES
 
 // Criando Sessão
@@ -22,12 +25,18 @@ app.use(
     saveUninitialized: true,
   })
 );
+// passport criando sessao
+app.use(passport.initialize())
+app.use(passport.session())
+
+// configurando flash
 app.use(flash());
 
 // criando MiddleWare
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash('error')
   next();
 });
 
@@ -58,6 +67,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Rotas admin
 app.use("/admin", admin);
+// Rotas usuarios
+app.use("/usuario", usuarios);
+
 
 // rotas principais
 app.get("/", (req, res) => {
@@ -70,6 +82,7 @@ app.get("/", (req, res) => {
       res.redirect("/404");
     });
 });
+// rota de postagem
 app.get("/postagem/:slug", (req, res) => {
   Postagem.findOne({ slug: req.params.slug }).then((postagem) => {
     if (postagem) {
@@ -83,6 +96,37 @@ app.get("/postagem/:slug", (req, res) => {
     res.redirect("/")
   })
 })
+// rosta de categorias
+app.get("/categorias", (req, res) => {
+  Categoria.find().lean().then((categorias) => {
+    res.render("categorias/index", { categorias: categorias })
+  })
+    .catch(() => {
+      req.flash("error_msg", "Houve um erro ao listar as categorias")
+      res.redirect("/");
+    })
+});
+// rota de acesso à alguma categoria
+
+app.get("/categorias/:slug", (req, res) => {
+  Categoria.findOne({ slug: req.params.slug }).lean().then((categoria) => {
+    if (categoria) {
+      Postagem.find({ categoria: categoria._id }).lean().then((postagens) => {
+        res.render("categorias/postagens", { postagens: postagens, categoria: categoria })
+      }).catch((err) => {
+        req.flash("error_msg", `houve um erro ao buscar a categoria!${err}`)
+        res.redirect("/")
+      })
+    } else {
+      req.flash("error_msg", "Esta categoria não existe")
+      res.redirect("/")
+    }
+  }).catch((err) => {
+    req.flash("error_msg", "Esta categoria não existe")
+    res.redirect("/")
+  })
+});
+// rota de erro
 app.get("/404", (req, res) => {
   res.send("Erro 404! :D");
 });
---
+++ config/auth.js
@@ -0,0 +1,43 @@
+const localStrategy = require("passport-local").Strategy
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+//model de usuário
+require("../models/Usuario")
+const Usuario = mongoose.model("usuarios")
+
+module.exports = function(passport) {
+  passport.use(new localStrategy({ usernameField: 'email', passwordField: 'senha' }, (email, senha, done) => {
+    Usuario.findOne({ email: email }).then((usuario) => {
+      if (
+        !usuario
+      ) {
+        return done(null, false, { message: "esta conta não existe" })
+      }
+      bcrypt.compare(senha, usuario.senha, (erro, batem) => {
+        if (batem) {
+          return done(null, usuario)
+        } else {
+          return done(null, false, { message: "Senha incorreta" })
+        }
+      })
+    })
+  })
+  )
+  passport.serializeUser((usuario, done) => {
+    if (!usuario || !usuario.id) {
+      return done(new Error('User object or user ID is missing'));
+    }
+    done(null, usuario.id);
+  });
+  passport.deserializeUser((id, done) => {
+    Usuario.findById(id).then((usuario) => {
+      if (!usuario) {
+        return done(null, false, { message: 'Usuário não encontrado' });
+      }
+      done(null, usuario);
+    }).catch((err) => {
+      done(err); // Pass the error to the done callback
+    });
+  });
+}
